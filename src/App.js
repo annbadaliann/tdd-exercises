0ba3fef2e6ef1edfd878634c49bfb4844d734b86
annbadaliann <annbadaliann2000@gmail.com>
2019-12-10 22:27:36 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h3>Test exercises for practicing TDD</h3>
+      <h2>Counter</h2>
+    </div>
+  );
+}
+
+export default App;
